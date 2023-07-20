787343b2890c90b583f3eddc8f845a2e2db11b53
anmvk58 <76772297+anmvk58@users.noreply.github.com>
2023-07-20 17:18:35 +0700

    Add Comment Models

--- blog/models.py
+++ blog/models.py
@@ -29,3 +29,9 @@ class Post(models.Model):
     content = models.TextField(validators=[MinLengthValidator(20)])
     author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True, related_name='posts')
     tags = models.ManyToManyField(Tag)
+
+class Comment(models.Model):
+    user_name = models.CharField(max_length=120)
+    user_email = models.CharField(max_length=120)
+    text = models.TextField(max_length=400)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
