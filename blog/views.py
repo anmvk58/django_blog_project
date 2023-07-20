9b04f4856cc1cd0f6a603b7715117742dc716cdd
anmvk58 <76772297+anmvk58@users.noreply.github.com>
2023-07-20 17:19:45 +0700

    Update view for new model Comment

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,13 @@
 from datetime import date
 from typing import Any, Dict
 from django.db.models.query import QuerySet
-
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import Post, Author, Tag
 from django.views.generic import ListView, DetailView
+from django.views import View
+from .forms import CommentForm
+from django.urls import reverse
 
 all_posts = []
 def get_date(post):
@@ -29,18 +31,54 @@ class AllPostView(ListView):
     ordering = ["-date"]
     context_object_name = "posts"
 
-class SinglePostView(DetailView):
-    template_name = 'blog/post-detail.html'
-    model = Post
+class SinglePostView(View):
+    def get(self, request, slug):
+        # post = Post.objects.get(slug=slug)
+        post = get_object_or_404(Post, slug=slug)
+        context = {
+            "post": post,
+            "post_tags" : post.tags.all(),
+            "form" : CommentForm()
+        }
+        return render(request, "blog/post-detail.html", context)
+    
+    def post(self, request, slug):
+        form = CommentForm(request.POST)
+        post = get_object_or_404(Post, slug=slug)
 
-    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
-        context =  super().get_context_data(**kwargs)
-        context["post_tags"] = self.object.tags.all()
-        return context
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
 
+            return HttpResponseRedirect(reverse("post-detail-page", args=[slug]))
+    
+        context = {
+            "post": post,
+            "post_tags" : post.tags.all(),
+            "form" : CommentForm()
+        }
+        return render(request, "blog/post-detail.html", context)
+
+#
+# This method deprecated because we need handle both get and post method !
+#
+# class SinglePostView(DetailView):
+#     template_name = 'blog/post-detail.html'
+#     model = Post
+
+#     def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+#         form = CommentForm()
+#         context =  super().get_context_data(**kwargs)
+#         context["post_tags"] = self.object.tags.all()
+#         context["form"] = form
+#         return context
+
+
+#
 # Create your views here.
 # Method belows is deprecated, Class inherit View/ListView above instead
-
+#
 # def starting_page(request):
 #     latest_post = Post.objects.all().order_by("-date")[:3]
 #     return render(request, 'blog/index.html', {
